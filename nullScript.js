cc744adef24de7aaa21efcc719f5f6a844185eff
notsudake <>
2022-03-15 18:48:20 +0000

    Initial commit

---
+++ nullScript.js
@@ -0,0 +1,194 @@
+console.log("would you mind NOT cheating? thanks")
+//OH LOOK, you decided to inspect the script! congratulations, you have earned the cheater award
+
+var quotes = [
+  "Explosion imminent. Evacuate the facility immidiately.",
+  "Reactor Explosion Timer destroyed.",
+  "All reactor core safeguards are now non-functional. Please prepare for reactor core meltdown.",
+  "Warning! Reactor core is at critical temperature.",
+  "Today is pizza day, so head on down to the cafeteria to grab yourself a hot slice!",
+  "Please prepare for emergency evacuation.",
+  "Reactor Explosion Uncertainty Emergency Preemption Protocol initiated: This facility will self destruct in two minutes.",
+  "Danger! Reactor meltdown, evacuate at once."
+]
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function utf8_to_b64( str ) {
+  return window.btoa(unescape(encodeURIComponent( str )));
+}
+
+function b64_to_utf8( str ) {
+  return decodeURIComponent(escape(window.atob( str )));
+}
+
+var coins = 0;
+var level = 0;
+var miners = 0;
+var announcers = 0;
+var playerHealth = 100;
+var bossHealth = 5000;
+var shield = 8;
+var doSave = false;
+var sPurchase = 0;
+const localStorage = window.localStorage;
+
+var coinCount = document.getElementById("coinCount")
+var challengeBtn = document.getElementById('challenge')
+var attack = document.getElementById('attack')
+var leveler = document.getElementById('levelup')
+var autoGet = document.getElementById('aabuy')
+
+function hurt(boss, amount) {
+  if (boss) { bossHealth -= amount;
+  document.getElementById("bosshp").innerHTML = `Boss health: <meter min="0" max="5000" low="1000" high="3000" optimum="5000" value="${bossHealth}"></meter> (${bossHealth} HP)`
+  if (bossHealth < 1) { bossHealth = 0; if (bossHealth === 0) win() }
+  } else {
+    playerHealth -= amount;
+  document.getElementById("playerhp").innerHTML = `Your health: <meter min="0" max="100" low="20" high="50" optimum="100" value="${playerHealth}"></meter> (${playerHealth} HP)`
+  if (playerHealth < 1) { playerHealth = 0; lose() }
+  }
+  
+}
+
+function saveGame() {
+  localStorage.setItem('coins', coins)
+  localStorage.setItem('miners', miners)
+  localStorage.setItem('announcers', announcers)
+}
+
+function loadGame(notif) {
+  if (!localStorage.getItem('coins')) { 
+    if (notif) { return alert('You have not saved yet!') }
+    else { return; }
+  }
+  coins = Number(localStorage.getItem('coins'))
+  miners = Number(localStorage.getItem('miners'))
+  announcers = Number(localStorage.getItem('announcers'))
+  sPurchase = Number(localStorage.getItem('sPurchase'))
+  coinCount.innerHTML = `Announcer coins: ${coins}`
+  
+}
+
+function ohNo() {
+  alert("You scream into the void: 1v1 or youre noob lmaoooo") 
+  alert("The ground starts to rumble...")
+  alert("Could it be.. the ɹǝɔunouu∀?")
+  alert("...")
+  alert("Sure enough, a few moments later, you see him manifest.")
+  document.getElementById("boss").src = "assets/recnuonna.png"
+  document.getElementById("bosshp").style = "display: block"
+  document.getElementById("challenge").style = "display: none"
+  document.getElementById("attack").style = "display: block"
+  document.getElementById("heal").style = "display: block"
+  document.getElementById("heald").style = "display: block"
+  document.getElementById("healp").style = "display: block"
+  document.getElementById("kill").style = "display: block"
+  document.getElementById("magic").style = "display: block"
+  document.getElementById("shield").style = "display: block"
+}
+
+function go() {
+  let hurtm1 = getRandom(5,27)
+  let hurtm2 = getRandom(50,80)
+    document.getElementById("quote").innerHTML = `You hit ɹǝɔunouu∀ and he loses ${hurtm2} HP. You lost ${hurtm1} HP.`; hurt(false, hurtm1)
+   ; hurt(true, hurtm2)
+}
+
+function magic() {
+  alert("Sorry i was too lazy to implement this so just dont use it for now")
+}
+
+function suicide() {
+  alert("No")
+}
+
+function shieldf() {
+  if (shield < 1) return;
+  shield -= 1
+  document.getElementById("shield").innerHTML = `Use shield (${shield}x)`
+  let blocker = getRandom(1,100)
+  if (blocker > 74) {
+    let hurtm = getRandom(8,13)
+    document.getElementById("quote").innerHTML = `You try to use the shield, but it doesn't work... You lost ${hurtm} HP.`
+    hurt(false, hurtm)
+  } else {
+    let hurtm = getRandom(160,290)
+    document.getElementById("quote").innerHTML = `You use the shield and it strikes back to the ɹǝɔunouu∀!`; hurt(true, hurtm)
+  }
+}
+
+function heal() {
+  level = level;
+  if (coins >= 1e12) {
+    if (playerHealth > 99) return;
+    playerHealth += 15; if (playerHealth > 99) playerHealth = 100
+    coins -= 1e12
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    document.getElementById("playerhp").innerHTML = `Your health: <meter min="0" max="100" low="20" high="50" optimum="100" value="${playerHealth}"></meter> (${playerHealth} HP)`
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function heal1() {
+  level = level;
+  if (coins >= 10e12) {
+    if (playerHealth > 99) return;
+    playerHealth += 35; if (playerHealth > 99) playerHealth = 100
+    coins -= 1e12
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    document.getElementById("playerhp").innerHTML = `Your health: <meter min="0" max="100" low="20" high="50" optimum="100" value="${playerHealth}"></meter> (${playerHealth} HP)`
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function heal2() {
+  level = level;
+  if (coins >= 100e12) {
+    if (playerHealth > 99) return;
+    playerHealth += 60; if (playerHealth > 99) playerHealth = 100
+    coins -= 1e12
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    document.getElementById("playerhp").innerHTML = `Your health: <meter min="0" max="100" low="20" high="50" optimum="100" value="${playerHealth}"></meter> (${playerHealth} HP)`
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function win() {
+   document.getElementById("bosshp").style = "display: none"
+   document.getElementById("boss").src = ""
+   alert("The ɹǝɔunouu∀ collapses...")
+   alert("Is it... finally over?")
+   alert("Seems so..")
+   alert("You can finally rest.")
+   saveGame()
+   localStorage.setItem("victory", true)
+   window.location.href = "credits.html"
+}
+
+function lose() {
+   document.getElementById("playerhp").style = "display: none"
+   alert("You collapse on the ground.")
+   alert("It seems that its too much to handle for you...")
+   alert("That is unfortunate.")
+   saveGame()
+   window.location.href = "index.html"
+}
+
+window.addEventListener('beforeunload', function (e) {
+  if (doSave) { saveGame() }
+  localStorage.setItem('lastPlayed', Date.now())
+  delete e['returnValue'];
+});
+
+window.addEventListener('load', function () {
+  loadGame(false)
+});
+
---
+++ script.js
@@ -0,0 +1,338 @@
+console.log("would you mind NOT cheating? thanks")
+//OH LOOK, you decided to inspect the script! congratulations, you have earned the cheater award
+
+var quotes = [
+  "Explosion imminent. Evacuate the facility immidiately.",
+  "Reactor Explosion Timer destroyed.",
+  "All reactor core safeguards are now non-functional. Please prepare for reactor core meltdown.",
+  "Warning! Reactor core is at critical temperature.",
+  "Today is pizza day, so head on down to the cafeteria to grab yourself a hot slice!",
+  "Please prepare for emergency evacuation.",
+  "Reactor Explosion Uncertainty Emergency Preemption Protocol initiated: This facility will self destruct in two minutes.",
+  "Danger! Reactor meltdown, evacuate at once."
+]
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function utf8_to_b64( str ) {
+  return window.btoa(unescape(encodeURIComponent( str )));
+}
+
+function b64_to_utf8( str ) {
+  return decodeURIComponent(escape(window.atob( str )));
+}
+
+
+var coins = 0;
+var level = 0;
+var miners = 0;
+var announcers = 0;
+var lastPlayed = 0;
+var darkMode = false;
+var doSave = false;
+var sPurchase = 0;
+const localStorage = window.localStorage;
+let vic = false
+if (localStorage.getItem("victory") === "true") { vic = true }
+
+var coinCount = document.getElementById("coinCount")
+var levelCount = document.getElementById("levelCount")
+var autoCount = document.getElementById("autoCount")
+var coinGet = document.getElementById('announce')
+var leveler = document.getElementById('levelup')
+var autoGet = document.getElementById('aabuy')
+
+function resetData() {
+  let choice = confirm("Are you sure you wanna do that? (There is a very low chance that your pizza permissions will be revoked. I cannot tell you the chance of such thing happening, but just be aware that it might happen.)")
+  if (choice) {
+    localStorage.clear()
+    location.reload()
+  }
+}
+
+function saveGame() {
+  localStorage.setItem('coins', coins)
+  localStorage.setItem('level', level)
+  localStorage.setItem('miners', miners)
+  localStorage.setItem('announcers', announcers)
+  localStorage.setItem('darkMode', darkMode)
+  localStorage.setItem('sPurchase', sPurchase)
+}
+
+
+function loadGame(notif) {
+  if (!localStorage.getItem('coins')) { 
+    if (notif) { return alert('You have not saved yet!') }
+    else { return; }
+  }
+  coins = Number(localStorage.getItem('coins'))
+  level = Number(localStorage.getItem('level'))
+  miners = Number(localStorage.getItem('miners'))
+  announcers = Number(localStorage.getItem('announcers'))
+  sPurchase = Number(localStorage.getItem('sPurchase'))
+  darkMode = localStorage.getItem('darkMode')
+  coinCount.innerHTML = `Announcer coins: ${coins}`
+  levelCount.innerHTML = `Level: ${level}`
+  autoCount.innerHTML = `Auto announcers: ${miners}`
+  coinGet.innerHTML = `Announce something (+${1+(level*2)} coins)`
+  leveler.innerHTML = `Level up (${Math.floor(200+(200*level))} coins)`
+  special()
+  getMedal(false)
+  var theme = document.getElementsByTagName('link')[0];
+  if (darkMode === "true") {darkMode = true} else {darkMode = false}
+  if (sPurchase > 1) {
+    theme.setAttribute('href', 'dark.css');
+    document.getElementById('darkmode').innerHTML = "You can't do it anymore."
+    return;
+  }
+  if (darkMode === true) {
+                theme.setAttribute('href', 'dark.css');
+                document.getElementById('darkmode').innerHTML = "disable dark mode"
+            } else {
+                theme.setAttribute('href', 'light.css');
+                document.getElementById('darkmode').innerHTML = "enable dark mode"
+            }
+  
+}
+
+function getMedal(notify) {
+  if (level < 100) {
+      announcers = 0
+      document.getElementById("medal").src = ""
+  }
+  if (level > 99 && level < 200) {
+    announcers = 1
+      document.getElementById("medal").src = "assets/bronze.png"
+      if (notify && level === 100) {alert('You have recieved a bronze medal!')}
+  }
+  if (level > 199 && level < 300) {
+      announcers = 2
+        document.getElementById("medal").src = "assets/silver.png"
+        if (notify && level === 200) {alert('You have recieved a silver medal!')}
+    }
+  if (level > 299) {
+    announcers = 3
+      document.getElementById("medal").src = "assets/gold.png"
+      if (notify && level === 300) {alert('You have recieved a gold medal!')}
+  }
+  if (vic) {
+    document.getElementById("medal").src = "assets/trophy.png"
+    document.body.style.backgroundImage = "url('./assets/gold.png')";
+  }
+}
+
+function special() {
+  if (level < 100 || (sPurchase === 1 && level > 99) || (sPurchase === 2 && level > 199) || sPurchase === 3) {
+    document.getElementById("spbuy1").style.display = "none";
+    document.getElementById("spbuy2").style.display = "none";
+    document.getElementById("spbuy3").style.display = "none";
+  } if (sPurchase === 0 && level > 99) {
+    document.getElementById("spbuy1").style.display = "block";
+    document.getElementById("spbuy2").style.display = "none";
+    document.getElementById("spbuy3").style.display = "none";
+  } if (sPurchase === 1 && level > 199) {
+    document.getElementById("spbuy1").style.display = "none";
+    document.getElementById("spbuy2").style.display = "block";
+    document.getElementById("spbuy3").style.display = "none";
+  } if (sPurchase === 2 && level > 299 && !vic) {
+    document.getElementById("spbuy1").style.display = "none";
+    document.getElementById("spbuy2").style.display = "none";
+    document.getElementById("spbuy3").style.display = "block";
+  }
+
+  if (sPurchase > 0) {
+    document.getElementById("title").classList.add("rainbow")
+    if (sPurchase > 1) {
+      document.body.style.backgroundImage = "url('./assets/announcer.png')";
+      theme.setAttribute('href', 'dark.css');
+    document.getElementById('darkmode').innerHTML = "You can't do it anymore."
+    
+      if (sPurchase > 2 && !vic) {
+        document.getElementById("nullify").style = "display: block"
+      }
+    }
+  }
+}
+
+function announce() {
+  coins += 1+(level*2)
+  coinCount.innerHTML = `Announcer coins: ${coins}`
+  document.getElementById('quote').innerHTML = quotes[getRandom(0, quotes.length - 1)]
+}
+
+function levelUp() {
+  let cost = Math.floor(200+(200*level))
+  if (coins >= cost) {
+    level += 1
+    coins -= cost
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    levelCount.innerHTML = `Level: ${level}`
+    coinGet.innerHTML = `Announce something (+${1+(level*2)} coins)`
+    leveler.innerHTML = `Level up (${Math.floor(200+(200*level))} coins)`
+    getMedal(true)
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function autoBuy() {
+  level = level;
+  if (coins >= 75) {
+    miners += 1
+    coins -= 75
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    autoCount.innerHTML = `Auto announcers: ${miners}`
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function autoBuy1() {
+  level = level;
+  if (coins >= 750) {
+    miners += 10
+    coins -= 750
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    autoCount.innerHTML = `Auto announcers: ${miners}`
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function autoBuy2() {
+  level = level;
+  if (coins >= 7500) {
+    miners += 100
+    coins -= 7500
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    autoCount.innerHTML = `Auto announcers: ${miners}`
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+function autoBuy3() {
+  level = level;
+  let rawInput = prompt("Enter the amount of auto announcers you want to buy:")
+  if (!rawInput || rawInput === "") {return;}
+  let bulkInput = Number(rawInput)
+  if (isNaN(bulkInput)) { return; }
+  if (confirm(`This will cost you ${bulkInput*75} coins`)) {
+    if (coins >= bulkInput*75) {
+    miners += bulkInput
+    coins -= bulkInput*75
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    autoCount.innerHTML = `Auto announcers: ${miners}`
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+  }
+  
+
+function dataExport() {
+  let dat = JSON.stringify({ coins: coins, level: level, miners: miners, lastPlayed: lastPlayed, darkMode: darkMode  })
+  let exp = utf8_to_b64(dat)
+  console.log(exp)
+  document.getElementById("exported").innerHTML = `Exported data: \n${exp}`
+}
+
+function dataImport() {
+  let input = prompt("Paste your save data here:")
+  let imp;
+  if (!input) { return; }
+  try {
+  imp = JSON.parse(b64_to_utf8(input))
+} catch (error) {
+  return alert("Data parse failed. Are you sure you've entered it correctly?")
+} finally {
+  coins = imp.coins
+  level = imp.level
+  miners = imp.miners
+  darkMode = imp.darkMode
+  var theme = document.getElementsByTagName('link')[0];
+  if (darkMode === true) {
+                theme.setAttribute('href', 'dark.css');
+                document.getElementById('darkmode').innerHTML = "disable dark mode"
+            } else {
+                theme.setAttribute('href', 'light.css');
+                document.getElementById('darkmode').innerHTML = "enable dark mode"
+            }
+  coinCount.innerHTML = `Announcer coins: ${coins}`
+  levelCount.innerHTML = `Level: ${level}`
+  autoCount.innerHTML = `Auto announcers: ${miners}`
+  coinGet.innerHTML = `Announce something (+${1+(level*2)} coins)`
+  leveler.innerHTML = `Level up (${Math.floor(200+(200*level))} coins)`
+  getMedal(false)
+  alert("Exported successfully!")
+} }
+
+function spBuy1() {
+  level = level;
+  if (coins >= 25e6) {
+    sPurchase = 1
+    coins -= 25e6
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    special()
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function spBuy2() {
+  level = level;
+  if (coins >= 5e10) {
+    sPurchase = 2
+    coins -= 5e10
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    special()
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function spBuy3() {
+  level = level;
+  if (coins >= 1e12) {
+    sPurchase = 3
+    coins -= 1e12
+    coinCount.innerHTML = `Announcer coins: ${coins}`
+    special()
+  } else {
+    alert("You don't have enough coins!")
+  }
+}
+
+function showCredits() {
+  alert("le funny game credits\n\nnotsu - Making the game\nnikothegamer - original game idea\nugnius991 - suggestions\nmarcus guy (cristal24) - suggestions\nWaviestBalloon - idfk but she made the announcer bot\nyou - being you")
+}
+
+
+window.addEventListener('beforeunload', function (e) {
+  if (doSave) { saveGame() }
+  localStorage.setItem('lastPlayed', Date.now())
+  delete e['returnValue'];
+});
+
+window.addEventListener('load', function () {
+  loadGame(false)
+  let lp = localStorage.getItem('lastPlayed');
+  lp = Math.floor(lp/1000)
+let dt = Math.floor(Date.now()/1000)
+  if (dt-lp > 4 && miners > 0) {
+    coins += miners * (dt - lp)
+    alert(`While you were away, you earned ${miners * (dt - lp)} announcer coins!`)
+  }
+});
+
+function autoget() {
+  coins += miners
+  coinCount.innerHTML = `Announcer coins: ${coins}`
+  if (miners > 0) { document.getElementById('quote').innerHTML = quotes[getRandom(0, quotes.length - 1)] }
+  setTimeout(autoget,1000);
+}
+
+autoget()
